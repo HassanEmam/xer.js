1c9f517b5a4f2ed87248997104686efac5d1cd23
HassanEmam <hassan.emam@hotmail.com>
2022-09-21 19:21:56 +0100

    Gantt WBS

--- examples/js/bundle.js
+++ examples/js/bundle.js
@@ -1547,6 +1547,7 @@ fileInput.addEventListener("change", (event) => {
     const wbss = parser.getWBS();
     const scheduleData = wbss.concat(activities);
     let container = document.getElementById("ganttChart");
+    container.innerHTML = "";
     let options = {
       container: container,
       dataDate: new Date(2022, 0, 15),
